b890e4c58f798f89091ee91952e93a5f917cac20
ismael-miguel <espertalhao04@hotmail.com>
2018-04-23 20:40:09 +0100

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -9,14 +9,14 @@ REM needs administrator rights
 REM we run net session to check the error returned
 net session > NUL 2>&1
 IF NOT %ERRORLEVEL% EQU 0 (
-    call :kill "You need to execute this as administrator"
+    call :kill 1 "You need to execute this as administrator"
 )
 
 REM this key is required
 REM we check if it exists before trying to run the code
 REG QUERY !REGKEY! > NUL 2>&1
 IF NOT %ERRORLEVEL% EQU 0 (
-    call :kill "Registry key not found"
+    call :kill 1 "Registry key not found"
 )
 
 REM fetches the data in the registry
@@ -25,38 +25,38 @@ for /f "tokens=2*" %%a in ('REG QUERY !REGKEY! /v BaseDir') do set "AppPath=%%~b
 REM ready to replace everything
 echo This will install the XignCode Bypasser.
 echo --------------------------------------------------------------------------------
-echo Detected the game installation on !AppPath!
-echo Executing from !CALLPATH!
+echo Game installation: !AppPath!
+echo Executing from: !CALLPATH!
 IF NOT EXIST "!AppPath!*" (
-    call :kill "!AppPath! doesn't exist or is empty"
+	call :kill 1 "!AppPath! doesn't exist or is empty"
 )
-echo If you wish to continue, press any key. Otherwise, close this window.
-pause >nul
+call :pause "If you wish to continue, press any key. Otherwise, close this window."
 
 REM call the patching function
-call :patch 32
-call :patch 64
+for %%b in (32,64) do call :patch %%b
 
 echo --------------------------------------------------------------------------------
 echo The XignCode Bypasser was successfully installed
-echo More in http://bnsbuddy.com/ and https://www.reddit.com/r/BladeAndSoulMods/
-echo Press any key to exit
+call :kill 0 "More in http://bnsbuddy.com/ and https://www.reddit.com/r/BladeAndSoulMods/"
 
+REM FUNCTION DECLARATION!
+
+:pause
+REM handles the pausing
+set a=%1
+echo !a:"=!
 pause >nul
-exit
+goto :eof
 
 :kill
 REM creates the exit messages
-REM https://ss64.com/nt/if.html
-IF NOT [%1] EQU [] (
-    set a=%1
-    echo !a:"=!
-    set a=
+IF NOT [%2] EQU [] (
+	set a=%2
+	echo !a:"=!
 )
-echo Press any key to close.
-pause >nul
-exit 1
-
+call :pause "Press any key to close."
+exit %1
+goto :eof
 
 :patch
 REM function to handle the patching
@@ -66,34 +66,35 @@ set dll=bsengine_Shipping
 set target=!AppPath!\bin
 
 IF NOT !bits! EQU 32 (
-    REM 64bit (or, if it ever exists for consumers, 128bit) paths need treatment -.-
-    set dll=!dll!!bits!
-    set target=!target!!bits!
+	REM 64bit (or, if it ever exists for consumers, 128bit) paths need treatment -.-
+	set dll=!dll!!bits!
+	set target=!target!!bits!
 )
 
 REM make sure the folder with the contents exist
 IF NOT EXIST "!folder!*" (
-    call :kill "Folder !bits! doesn't exist or is empty"
+	call :kill 1 "Folder !bits! doesn't exist or is empty"
 )
 
 REM it exists, confirm the remaining
 IF NOT EXIST "!folder!XignCode\*" (
-    call :kill "Folder !bits!\XignCode doesn't exist or is empty"
+	call :kill 1 "Folder !bits!\XignCode doesn't exist or is empty"
 )
 IF NOT EXIST "!folder!!dll!.dll" (
-    call :kill "File !bits!\!dll!.dll doesn't exist"
+	call :kill 1 "File !bits!\!dll!.dll doesn't exist"
 )
 
-echo Folder !bits! found, patching...
+echo Folder !bits! found, copying files ...
 
 REM begin copying the directory
-xcopy "!folder!XignCode" "!target!\XignCode\" /i /s /q /y >nul
+xcopy "!folder!XignCode" "!target!\XignCode\" /i /s /q /y >nul 2>&1
 IF NOT %ERRORLEVEL% EQU 0 (
-    call :kill "Error (%ERRORLEVEL%) while copying the folder !bits!\XignCode"
+    call :kill 1 "Error (%ERRORLEVEL%) while copying the folder !bits!\XignCode"
 )
 
 REM copy the dll file
-copy "!folder!!dll!.dll" "!target!\XignCode\!dll!.dll" /b /y >nul
+copy "!folder!!dll!.dll" "!target!\XignCode\!dll!.dll" /b /y >nul 2>&1
 IF NOT %ERRORLEVEL% EQU 0 (
-    call :kill "Error (%ERRORLEVEL%) while copying the file !bits!\!dll!.dll"
+    call :kill 1 "Error (%ERRORLEVEL%) while copying the file !bits!\!dll!.dll"
 )
+goto :eof
